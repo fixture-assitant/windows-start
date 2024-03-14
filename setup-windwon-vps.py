ec2b1ae7d5266995165b9c5b98d7e2bf269d3c2c
fixture-assitant <163410955+fixture-assitant@users.noreply.github.com>
2024-03-14 18:39:01 +0700

    !

--- setup-windwon-vps.py
+++ setup-windwon-vps.py
@@ -1,1 +1,7 @@
-print("Hello, world!")
+import os
+
+def uninstall_internet_explorer():
+    os.system("powershell \"Get-WindowsFeature -Name 'Internet-Explorer-Optional-amd64' | Uninstall-WindowsFeature -Remove\"")
+
+if __name__ == "__main__":
+    uninstall_internet_explorer()
