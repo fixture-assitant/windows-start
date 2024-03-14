554cee9e9a25139ebac379bb6806b65624a2b5fe
fixture-assitant <163410955+fixture-assitant@users.noreply.github.com>
2024-03-14 18:18:04 +0700

    update

--- setup-windwon-vps.ps1
+++ setup-windwon-vps.ps1
@@ -1,17 +1,40 @@
+# Function to write log both to file and console
+Function Write-Log {
+    Param ([string]$logMessage)
+    
+    $logTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logString = "$logTime: $logMessage"
+    # Write to console
+    Write-Host $logString
+}
 
-Write-Host "Hello World"
-
-# Remove Keys
+# Remove existing product keys
+Write-Log "Removing existing product keys..."
 slmgr /upk
 
-# Find Available Target Editions
+# Get available target editions
+Write-Log "Retrieving available target editions..."
 DISM.exe /Online /Get-TargetEditions
 
 # Convert Server Standard 2022 Evaluation to Server Standard 2022
+Write-Log "Converting to Server Standard 2022..."
 DISM /online /Set-Edition:ServerDatacenter /ProductKey:WX4NM-KYWYW-QJJR4-XV3QB-6VM33 /AcceptEula
 
-# How To Activate 
+# Input new product key
+Write-Log "Applying new product key..."
 slmgr /ipk WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+
+# Set Key Management Service server
+Write-Log "Setting Key Management Service server..."
 slmgr /skms kms.digiboy.ir
+
+# Activate Windows
+Write-Log "Activating Windows..."
 slmgr /ato
+
+# Display license information
+Write-Log "Displaying license information..."
 slmgr /dlv
+
+# Final message
+Write-Log "Operation Completed."
